1d128af1c9af5d0c7a575e0a7f48398974f81cd2
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-10-26 17:26:14 -0300

    upPingBallMej.java

--- core/src/com/mygdx/game/PingBallMejora.java
+++ core/src/com/mygdx/game/PingBallMejora.java
@@ -6,10 +6,20 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
 public class PingBallMejora extends PingBall{
 	private Color color = Color.RED;
-    private boolean destroyed = false;
+	private boolean destroyed = false;
 	
-	public PingBallMejora(int x, int y, float size, float xSpeed, float ySpeed, boolean iniciaQuieto) {
+	public PingBallMejora(int x, int y, float size, float xSpeed, float ySpeed, boolean iniciaQuieto, boolean destroyed) {
 		super(x, y, size, xSpeed, ySpeed, iniciaQuieto);
+		this.setDestroyed(destroyed);
+
+	}
+	public boolean isDestroyed() {
+		return destroyed;
+	}
+
+	public boolean setDestroyed(boolean destroyed) {
+		this.destroyed = destroyed;
+		return destroyed;
 	}
 
 	public void draw(ShapeRenderer shape) {
@@ -25,7 +35,7 @@ public class PingBallMejora extends PingBall{
 	public void checkCollision(Paddle paddle) {
 		if (collidesWith(paddle)) {
 			color = Color.GREEN;
-	        destroyed = true;
+			setDestroyed(true);
 		} else {
 			color = Color.RED;
 
@@ -44,7 +54,7 @@ public class PingBallMejora extends PingBall{
 	public void destroy() {
 		if(destroyed = true) {
 			setX(-1000);
-	        setY(-1000);
+			setY(-1000);
 		}
 	}
 	
