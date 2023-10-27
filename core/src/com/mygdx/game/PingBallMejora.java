3fd303221ee3eebc0976e7853f312d71ca9d73a7
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-10-27 12:37:37 -0300

    SonidoPingBallMejora.java

--- core/src/com/mygdx/game/PingBallMejora.java
+++ core/src/com/mygdx/game/PingBallMejora.java
@@ -3,6 +3,8 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.audio.Sound;
+
 
 public class PingBallMejora extends PingBall{
 	private Color color = Color.RED;
@@ -10,6 +12,8 @@ public class PingBallMejora extends PingBall{
 	
 	public PingBallMejora(int x, int y, float size, float xSpeed, float ySpeed, boolean iniciaQuieto) {
 		super(x, y, size, xSpeed, ySpeed, iniciaQuieto);
+		sonidoColision = Gdx.audio.newSound(Gdx.files.internal("choqueBall.mp3")); // Inicializa sonidoColision
+
 	}
 
 	public void draw(ShapeRenderer shape) {
@@ -38,7 +42,20 @@ public class PingBallMejora extends PingBall{
 		return intersectaX && intersectaY;
 	}
 
+	private boolean collidesWith(Block bb) {
+		boolean intersectaX = (bb.x + bb.width >= getX() - getSize()) && (bb.x <= getX() + getSize());
+		boolean intersectaY = (bb.y + bb.height >= getY() - getSize()) && (bb.y <= getY() + getSize());
+		return intersectaX && intersectaY;
+	}
+
 	public void checkCollision(Block block) {
+		if(collidesWith(block)) {
+			sonidoColision.play();
+			ySpeed = -ySpeed;
+			block.destroyed = true;
+
+			
+		}
 	}
 	
 	public void destroy() {
