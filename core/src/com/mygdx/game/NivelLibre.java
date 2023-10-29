c8d0beaa2dad5fdc7a2b8264869ee5fead89b985
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-10-29 20:27:25 -0300

    ComentNivelLibre.java

--- core/src/com/mygdx/game/NivelLibre.java
+++ core/src/com/mygdx/game/NivelLibre.java
@@ -26,12 +26,15 @@ public class NivelLibre implements Nivel{
 	int yAleatorio = random.nextInt(juegoAlto + 2500);
 
 	public NivelLibre() {
+		// Método constructor para inicializar el nivel libre
 		nivel = 1;
-		crearBloques(2+nivel);
+		crearBloques(2+nivel); // Crear bloques para el nivel
 
 		shape = new ShapeRenderer();
 	    shape.setAutoShapeType(true);
 		
+		// Inicializar la pelota, la pelota de mejora y el paddle con posiciones y tamaños específicos
+
 		ball = new PingBallNormal(Gdx.graphics.getWidth()/2-10, 41, 10, 5, 7, true);
 		ballMejora = new PingBallMejora(xAleatorio, yAleatorio, 7, 7, 10, true);
 		pad = new Paddle(Gdx.graphics.getWidth()/2-50,40,100,10);
@@ -39,6 +42,7 @@ public class NivelLibre implements Nivel{
 		vidas = 3;
 		puntaje = 0;
 	}
+	//método para formar los bloques por fila en la parte superior de la ventana
 	public void crearBloques(int filas) {
 		int blockWidth = 70;
 		int blockHeight = 26;
@@ -50,7 +54,9 @@ public class NivelLibre implements Nivel{
 			}
 		}
 	}
-	
+
+	// Método para comprobar si el jugador ha perdido todas sus vidas
+
 	public boolean perder() {
 		if (vidas<=0) {
 			return true;
@@ -58,7 +64,9 @@ public class NivelLibre implements Nivel{
 		else 
 			return false;
 	}
-	
+
+	// Método para comprobar la posición de la pelota y actualizar el número de vidas si la pelota se sale de la pantalla
+
 	public void comprobarPelota() {
 		if (ball.getY()<0) {
 			int xAleatorio = random.nextInt(juegoAncho - 40); // Ajusta el valor máximo para que no se salga de los bordes
@@ -69,6 +77,8 @@ public class NivelLibre implements Nivel{
 		}
 	}
 
+	// Método para actualizar la posición de la pelota y la acción de empezar a moverse al presionar la tecla de espacio
+
 	public void actualizarLugar() {
 		if (ball.estaQuieto()) {
 			ball.setXY(pad.getX()+pad.getWidth()/2-5, pad.getY()+pad.getHeight()+11);
@@ -78,7 +88,8 @@ public class NivelLibre implements Nivel{
 			ballMejora.update();
 		}
 	}
-	
+
+	// Método para comprobar colisiones entre las pelotas y el paddle, y realizar acciones según la colisión
 	public void colisionesPelotas() {
 	    boolean colisionBallMejora = ballMejora.checkCollision(pad);
 		ball.checkCollision(pad);
@@ -94,14 +105,17 @@ public class NivelLibre implements Nivel{
 			
 		}
 	}
-	
+
+	// Método para dibujar las pelotas en el juego
+
 	public void dibujarPelotas() {
 		shape.begin(ShapeRenderer.ShapeType.Filled);
 		ball.draw(shape);
 		ballMejora.draw(shape);
 		shape.end();
 	}
-	
+
+	// Método para verificar si el nivel actual está completo y preparar el siguiente nivel
 	public boolean nivelCompleto(){
 		if (blocks.isEmpty()) {
 			nivel++;
@@ -114,14 +128,14 @@ public class NivelLibre implements Nivel{
 		return false;
 	}
 	
-
+	// Método para dibujar el paddle en el juego
 	public void dibujarPaddle() {
 		shape.begin(ShapeRenderer.ShapeType.Filled);
 		pad.draw(shape);
 		shape.end();
 	}
-	
 
+	// Método para actualizar los bloques en el juego y el puntaje del jugador
 	public void actualizarBloques() {
 		for (int i = 0; i < blocks.size(); i++) {
 			Block b = blocks.get(i);
@@ -132,8 +146,8 @@ public class NivelLibre implements Nivel{
 			}
 		}
 	}
-	
 
+	// Método para dibujar los bloques en el juego
 	public void dibujarBloques() {
 		shape.begin(ShapeRenderer.ShapeType.Filled);
 		for (Block b : blocks) {
@@ -143,7 +157,7 @@ public class NivelLibre implements Nivel{
 		shape.end();
 	}
 	
-	
+	//getter par el puntaje y vidas
     public int getPuntaje() {
         return puntaje;
     }
@@ -151,7 +165,8 @@ public class NivelLibre implements Nivel{
     public int getVidas() {
         return vidas;
     }
-    
+
+	//ventaja mínima para este nivel en solo aumentar el tmaño del pingBall
     public void mejoras() {        
         ball.setSize(ball.getSize() + 5); 
     }
