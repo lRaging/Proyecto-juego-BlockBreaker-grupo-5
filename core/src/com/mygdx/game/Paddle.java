ab2812b21a3bc5231e03a955c1fed74cadff1e20
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-10-29 19:51:05 -0300

    comentPaddle.java

--- core/src/com/mygdx/game/Paddle.java
+++ core/src/com/mygdx/game/Paddle.java
@@ -6,13 +6,17 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
 
+//clase para definir el paddle con sus posicion inicial, ancho, largo y el rectangulo para interactuar con otros elementos
 public class Paddle {
     private int x = 20;
     private int y = 20;
     private int width = 100;
     private int height = 10;
     private Rectangle area;
-    
+
+
+
+//constructor para inicializar los atributos correspondientes	
     public Paddle(int x, int y, int ancho, int alto) {
     	this.x = x;
     	this.y= y;
@@ -20,7 +24,8 @@ public class Paddle {
     	height = alto;
         area = new Rectangle(x, y, width, height);
     }
-     
+
+	//getters para su composicion y setter para el ancho
     public int getX() {return x;}
 	public int getY() {return y;}
 	public int getWidth() {return width;}
@@ -29,6 +34,7 @@ public class Paddle {
 		this.width = width;
 	}
 
+	//metodo para dibujar la figura y color, ademas las teclas para comenzar el movieminto de esta
 	public void draw(ShapeRenderer shape){	
         shape.setColor(Color.BLUE);
         int x2 = x; //= Gdx.input.getX();
@@ -40,7 +46,8 @@ public class Paddle {
         }
         shape.rect(x, y, width, height);
     }
-    
+
+	//getter del area que ocupará el rectángulo
     public Rectangle getArea() {
         return area;
     }
