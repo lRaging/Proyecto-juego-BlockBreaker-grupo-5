dd0360b241ccc1cb274cb3f4438ca7d722917d35
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-10-29 17:35:01 -0300

    comentPingBallNormal.java

--- core/src/com/mygdx/game/PingBallNormal.java
+++ core/src/com/mygdx/game/PingBallNormal.java
@@ -5,22 +5,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 
-
+//clase hija de la clase abstracta pingball que comparte los mismos métodos con pingBallMejora pero distinto comportamiento
 public class PingBallNormal extends PingBall {
 	private Color color = Color.WHITE;
 
+
+	//constructor para obtener las mismas medidas, velocidad, la posicion inicla de estar quieto de la clase padre PingBall, y se incluye el sonido a implementar
 	public PingBallNormal(int x, int y, float size, float xSpeed, float ySpeed, boolean iniciaQuieto) {
 		super(x, y, size, xSpeed, ySpeed, iniciaQuieto);
 		sonidoColision = Gdx.audio.newSound(Gdx.files.internal("choqueBall.mp3")); // Inicializa sonidoColision
 	}
 
 	@Override
+	//Se comienza por dibujar el circulo con su tamaño que representa el Pingball y el color de ella
 	public void draw(ShapeRenderer shape) {
 		shape.setColor(color);
 		shape.circle(getX(), getY(), getSize());
 	}
 
 	@Override
+	//asegurar de que el Pingball inicialize su posicion dentro del juego, y si se colisiona con los bordes, que se produzca un "rebote"
 	public void update() {
 		if (estaQuieto()) return;
 		setX(getX() + xSpeed);
@@ -34,6 +38,7 @@ public class PingBallNormal extends PingBall {
 	}
 
 	@Override
+	//si se colisiona con el paddle en la parte inferior, debe rebotarse con un cambio de color
 	public boolean checkCollision(Paddle paddle) {
 		if (collidesWith(paddle)) {
 			color = Color.GREEN;
@@ -45,6 +50,7 @@ public class PingBallNormal extends PingBall {
 		}
 	}
 
+	//retorna si se cumple la coincidencia de coordenadas y colision del paddle con el PingBall
 	private boolean collidesWith(Paddle pp) {
 		boolean intersectaX = (pp.getX() + pp.getWidth() >= getX() - getSize()) && (pp.getX() <= getX() + getSize());
 		boolean intersectaY = (pp.getY() + pp.getHeight() >= getY() - getSize()) && (pp.getY() <= getY() + getSize());
@@ -52,6 +58,8 @@ public class PingBallNormal extends PingBall {
 	}
 
 	@Override
+
+	//Si el pingball choca con el block, debe ser destruida y rebotada
 	public void checkCollision(Block block) {
 		if (collidesWith(block)) {
 			sonidoColision.play();
@@ -59,7 +67,7 @@ public class PingBallNormal extends PingBall {
 			block.destroyed = true;
 		}
 	}
-
+//retorna si se cumple la coincidencia de coordenadas y colision del block con el PingBall
 	private boolean collidesWith(Block bb) {
 		boolean intersectaX = (bb.x + bb.width >= getX() - getSize()) && (bb.x <= getX() + getSize());
 		boolean intersectaY = (bb.y + bb.height >= getY() - getSize()) && (bb.y <= getY() + getSize());
