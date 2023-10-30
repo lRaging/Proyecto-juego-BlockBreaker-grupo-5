cf1ef4237cfb35f0c4993585d0ae5d00f5eef9ce
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-10-29 21:01:10 -0300

    comentBlockBreakerMenu.java

--- core/src/com/mygdx/game/BlockBreakerMenu.java
+++ core/src/com/mygdx/game/BlockBreakerMenu.java
@@ -4,27 +4,32 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class BlockBreakerMenu extends Game {
-
+public class BlockBreakerMenu extends Game 
+    // Variables de instancia para el menú del juego
     private SpriteBatch batch;
     private BitmapFont font;
     private int higherScore;
 
+    // Método para crear el menú del juego
     public void create() {
         batch = new SpriteBatch();
         font = new BitmapFont();
+        // Configura la pantalla de inicio como la pantalla actual del juego
         this.setScreen(new MainMenuScreen(this));
     }
 
+    // Método para renderizar el menú del juego
     public void render() {
         super.render(); // important!
     }
 
+    // Método para liberar los recursos utilizados por el menú del juego
     public void dispose() {
         batch.dispose();
         font.dispose();
     }
 
+//setter y getter para los atributos de la clase
     public SpriteBatch getBatch() {
         return batch;
     }
