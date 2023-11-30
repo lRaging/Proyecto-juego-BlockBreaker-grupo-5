dde196866def178c35e1c3b7e22d4f10bb3498a7
kavonkerm <127423218+kavonkerm@users.noreply.github.com>
2023-11-30 15:06:05 -0300

    ImplementaciónStrategyVelocidad

---
+++ VelocidadFacilStrategy.java
@@ -0,0 +1,17 @@
+package com.mygdx.game;
+
+public class VelocidadFacilStrategy implements VelocidadPingBallStrategy {
+
+	@Override
+	public int obtenerVelocidadX() {
+		// TODO Auto-generated method stub
+		return 5;
+	}
+
+	@Override
+	public int obtenerVelocidadY() {
+		// TODO Auto-generated method stub
+		return 7;
+	}
+
+}
